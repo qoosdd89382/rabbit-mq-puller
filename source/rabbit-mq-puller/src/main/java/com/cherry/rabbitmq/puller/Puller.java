79bf53153f773cb55f6b2ade023f46fa736f04a5
Cherry <cherry@webcomm.com.tw>
2023-08-16 09:25:58 +0800

    puller will manually ack message

--- source/rabbit-mq-puller/src/main/java/com/cherry/rabbitmq/puller/Puller.java
+++ source/rabbit-mq-puller/src/main/java/com/cherry/rabbitmq/puller/Puller.java
@@ -2,6 +2,8 @@ package com.cherry.rabbitmq.puller;
 
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.connection.Connection;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import com.rabbitmq.client.GetResponse;
@@ -35,26 +37,35 @@ public class Puller {
         try (Connection connection = connectionFactory.createConnection();
              Channel channel = connection.createChannel(false);
         ) {
-
             boolean hasResponse = this.pullMessage(channel);
             while (hasResponse) {
                 hasResponse = this.pullMessage(channel);
             }
-
         }
     }
 
     private boolean pullMessage(Channel channel) throws IOException {
-        GetResponse response = channel.basicGet(Constants.QUEUE_NAME_FOR_PULLING, true);
-        boolean hasResponse = response != null;
-        if (hasResponse) {
-            String message = new String(response.getBody());
-            System.out.println("Pulled: " + message);
-        } else {
-            System.out.println("No messages available");
-            System.out.println("Consumer turns off");
+        boolean shouldRequeue = true;
+
+        GetResponse response = channel.basicGet(Constants.QUEUE_NAME_FOR_PULLING, false);
+        try {
+            boolean hasResponse = response != null;
+            if (hasResponse) {
+                String message = new String(response.getBody());
+                System.out.println("Pulled: " + message);
+
+                long deliveryTag = response.getEnvelope().getDeliveryTag();
+                channel.basicAck(deliveryTag, false);
+            } else {
+                System.out.println("No messages available");
+                System.out.println("Consumer turns off");
+            }
+            return hasResponse;
+        } catch (Exception e) {
+            System.out.println(" [x] Receiver1 error processing message: " + e.getMessage());
+            channel.basicNack(response.getEnvelope().getDeliveryTag(), false, shouldRequeue);
+            throw e;
         }
-        return hasResponse;
     }
 
 }
