394fa97833ed67705ade69777b14de02d23bf246
Cherry <cherry@webcomm.com.tw>
2023-08-16 11:57:48 +0800

    keep conncection for pull queue inspector

--- source/rabbit-mq-inspector/src/main/java/com/cherry/rabbitmq/inspector/InspectListener.java
+++ source/rabbit-mq-inspector/src/main/java/com/cherry/rabbitmq/inspector/InspectListener.java
@@ -4,24 +4,19 @@ import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.connection.Connection;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
-
 @Component
-public class Inspector {
+public class InspectListener {
 
-    private final ConnectionFactory connectionFactory;
+    private final PullQueueInspector inspector;
     private final PullClient puller;
 
-    public Inspector(
-            ConnectionFactory connectionFactory
+    public InspectListener(
+            PullQueueInspector inspector
             , PullClient puller
     ) {
-        this.connectionFactory = connectionFactory;
+        this.inspector = inspector;
         this.puller = puller;
     }
 
@@ -33,7 +28,7 @@ public class Inspector {
             String receivedMessage = new String(message.getBody());
             System.out.println(" [new] Receiver Received '" + receivedMessage + "'");
 
-            AMQP.Queue.DeclareOk declareOk = this.inspectPassiveForPulling();
+            AMQP.Queue.DeclareOk declareOk = inspector.inspectPassiveForPulling();
             int messageCount = declareOk.getMessageCount();
             System.out.println(messageCount);
             if (messageCount > 10) {
@@ -49,20 +44,6 @@ public class Inspector {
             System.out.println(" [x] Receiver1 error processing message: " + e.getMessage());
 
             channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, shouldRequeue);
-        }
-    }
-
-    public AMQP.Queue.DeclareOk inspectPassiveForPulling() throws IOException, TimeoutException {
-        try (Connection connection = connectionFactory.createConnection();
-             Channel channel = connection.createChannel(false);
-        ) {
-            return channel.queueDeclarePassive(Constants.QUEUE_NAME_FOR_PULLING);
-        }
-    }
-
-    public static AMQP.Queue.DeclareOk inspectPassiveForPulling(Channel channel) throws IOException, TimeoutException {
-        try (channel) {
-            return channel.queueDeclarePassive(Constants.QUEUE_NAME_FOR_PULLING);
         }
     }
 }
